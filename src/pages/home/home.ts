e8a4992783fd2609ebb9f1a0302d6a0d0f7f6045
tamezfer <fervillatamez@gmail.com>
2019-02-07 16:18:00 +0100

    Se acabo quiz

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  casas = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+          public http: HttpClient) {
+            this.http.get('/v1/api/pin-data?url=/s-renta-inmuebles/guadalajara-y-zona-metro/v1c1098l10567p1&geo=(21.10631012145462,-102.42214381725364),(20.21712862656199,-104.32387728274637)')
+            .subscribe(data => {
+              console.log(JSON.stringify(data['ads'], null, 3));
+              if(data.hasOwnProperty('ads')) {
+                this.casas = data['ads'];
+              }
+            }, error => {
+              console.log(JSON.stringify(error));
+            });
   }
-
 }
+
+//imagen del inmueble esta en pictures[0].url
+//Precio esta en price.formatttedAmount
+//Colonia esta en geo.displayName
+//Municipio esta en geo.name
